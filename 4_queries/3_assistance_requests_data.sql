a8656856a6ad262feab400c913fa77a7db25ce7c
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2021-11-24 06:33:02 +0000

    Added query to display average assistance time

--- 4_queries/3_assistance_requests_data.sql
+++ 4_queries/3_assistance_requests_data.sql
@@ -4,7 +4,3 @@ JOIN teachers ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN assignments ON assignment_id = assignments.id
 ORDER BY duration;
-
-
-
--- 3_assistance_requests_data
