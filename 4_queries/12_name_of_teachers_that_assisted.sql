9c2d4bf763ae0333092a979637769c99183f9232
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2021-11-24 07:41:15 +0000

    Added rest of queries from BootcampX Queries 4 | Forgot to commit everytime oops

---
+++ 4_queries/12_name_of_teachers_that_assisted.sql
@@ -0,0 +1,8 @@
+
+SELECT DISTINCT teachers.name as teachers, cohorts.name as name
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = 'JUL02'
+ORDER BY teachers;
